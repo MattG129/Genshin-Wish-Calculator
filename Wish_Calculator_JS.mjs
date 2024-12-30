34ed78fb1c9cb075c130b8badcc98d8e14c9f9df
MattG129 <61367937+MattG129@users.noreply.github.com>
2024-12-30 16:02:52 -0500

    Added calcs for hoyo lab daily check-ins.

--- Wish_Calculator_JS.mjs
+++ Wish_Calculator_JS.mjs
@@ -211,6 +211,32 @@ function SavingsCalculator(WishConfig) {
                     Primos += (WishConfig.SecondaryEventsCompletable - WishConfig.SecondaryEventsCompleted) * 420
                 };
             };
+
+            if (WishConfig.UsingHoyoLabCheckin) {
+                Primos += 60*(LastBannerInfo.MonthDiff + 1); // +1 for the current month.
+                
+                // Subtract based on the amount of primos we could claim.
+                if (Today.getDate() >= 18) {
+                    Primos -= 60
+                }
+                else if (Today.getDate() >= 11) {
+                    Primos -= 40
+                }
+                else if (Today.getDate() >= 4) {
+                    Primos -= 20
+                };
+
+                // Subtracts primos based on how many could actually be claimed by the banner end date.
+                if (LastBannerInfo.BannerEndDate.getDate() < 4) {
+                    Primos -= 60
+                }
+                else if (LastBannerInfo.BannerEndDate.getDate() < 11) {
+                    Primos -= 40
+                }
+                else if (LastBannerInfo.BannerEndDate.getDate() < 18) {
+                    Primos -= 20
+                };
+            };
         };
     }
 
