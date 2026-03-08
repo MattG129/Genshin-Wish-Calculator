f88eab2bb406a99c5b112b9c0d3c478903df5032
MattG129 <61367937+MattG129@users.noreply.github.com>
2026-03-08 00:40:13 -0500

    Updated skip logic to make it work with normal mode.

--- Scripts/Events.js
+++ Scripts/Events.js
@@ -1,12 +1,19 @@
 function AreFirstPhaseEventFieldsActive() {
-    if (BannerInfo[CurrentBannerVal].Phase == 1 && $('#WishMode').val() != WishModes.Simple.value) {
-        for (let Banner of $('.WishPlanBannerEnd')) {
+    if (BannerInfo[CurrentBannerVal].Phase == 1) {
+        if ($('#WishMode').val() == WishModes.Normal.value && $('#BannerEnd').val() == CurrentBannerVal) { // Only adding the skip logic for the advanced mode for now.
+            return 1;
+        }
+        else if ($('#WishMode').val() == WishModes.Advanced.value) {
+            for (let Banner of $('.WishPlanBannerEnd')) {
 
-            if ($(Banner).val() == CurrentBannerVal && !$(Banner).hasClass('DisabledWishPlanRow')) {
-                return 1;
+                if ($(Banner).val() == CurrentBannerVal && !$(Banner).hasClass('DisabledWishPlanRow')) {
+                    return 1;
+                };
             };
         };
     };
 
     return 0;
 };
+
+$(function() {$('#BannerEnd').change(function() {ToggleFirstPhaseEventFields()})});
